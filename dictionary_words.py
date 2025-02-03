be801686d0e3168d1d84243b1292be4e0515ea8a
Killian <killian.delamarre-ruer@students.dominican.edu>
2025-02-03 07:18:52 -0800

    added the other functions

--- dictionary_words.py
+++ dictionary_words.py
@@ -1,17 +1,34 @@
-import sys
 import random
+import sys
 
+def load_words(file_path="/usr/share/dict/words"):
+    """Read words from the dictionary file and return them as a list."""
+    with open(file_path, "r") as file:
+        words = [line.strip() for line in file.readlines()]
+    return words
 
-def random_set(number):
-    """Open file and read words. Join words into a sentence."""
-    with open('/usr/share/dict/words', 'r') as file:
-        words = file.readlines()  
-    words = [word.strip() for word in words]
-    random_words = random.sample(words, number)  
-    sentence = ' '.join(random_words) + '.'  
-    return sentence  
+def generate_sentence(word_list, num_words):
+    """Select a random set of words from the list and form a sentence."""
+    random_words = random.sample(word_list, num_words)
+    return " ".join(random_words) + "."
 
+def main():
+    """Main function to execute the script."""
+    if len(sys.argv) != 2:
+        print("Usage: python3 dictionary_words.py <number_of_words>")
+        sys.exit(1)
+    
+    try:
+        num_words = int(sys.argv[1])
+        if num_words < 1:
+            raise ValueError
+    except ValueError:
+        print("Please enter a valid positive integer for the number of words.")
+        sys.exit(1)
+    
+    words = load_words()
+    sentence = generate_sentence(words, num_words)
+    print(sentence)
 
 if __name__ == "__main__":
-    number = int(sys.argv[1])
-    print(random_set(number))
+    main()

db43b8341c5f81b5594c012b5b5cd75cb80f4460
Killian <killian.delamarre-ruer@students.dominican.edu>
2025-02-03 07:16:45 -0800

    updated dictionary words script

--- dictionary_words.py
+++ dictionary_words.py
@@ -1,20 +1,9 @@
 import sys
 import random
 
-"""
--Write the dictonary_words.py script:
--read in the words file
--select a random set of words from the file and store in a data type
--put the number of words requested together into a “sentence”
--output your sentence
-
-"""
-
 
 def random_set(number):
-    """Open file and read the words. Remove extra space from word list. Select number of random words. 
-    Join words into a sentence and add a period at the end. 
-    """
+    """Open file and read words. Join words into a sentence."""
     with open('/usr/share/dict/words', 'r') as file:
         words = file.readlines()  
     words = [word.strip() for word in words]

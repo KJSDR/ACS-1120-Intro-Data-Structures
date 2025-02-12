9b879aca9986376ce60b2b4c75bc4dec6a6d11e8
Killian <killian.delamarre-ruer@students.dominican.edu>
2025-02-11 23:14:01 -0800

    mod 5

--- Code/app.py
+++ Code/app.py
@@ -1,29 +1,25 @@
 """Main script, uses other modules to generate sentences."""
 from flask import Flask
-from dictogram import Dictogram
-
-
+from .histogram import histogram # adding . to import from same directory
+import random
 
 app = Flask(__name__)
 
 # TODO: Initialize your histogram, hash table, or markov chain here.
 # Any code placed here will run only once, when the server starts.
-
-word_list = "one fish two fish red fish blue fish".split()
-histogram = Dictogram(word_list)
-
-
-def generate_sentence(word_count=5):
-    """Generate a sentence by randomly selecting weighted words."""
-    return " ".join(histogram.sample() for _ in range(word_count))
-
+hist = histogram("Code/flowersofevil.txt") # creates histo from my source text
 
 @app.route("/")
 def home():
     """Route that returns a web page containing the generated text."""
-    sentence = generate_sentence()
-    return f"<p>{sentence.capitalize()}.</p>"
+    words = list(hist.keys()) # getting all words from histo as a list
+    sentence_length = random.randint(20, 30)
 
+    # generating rand words & joining w spaces
+    random_words = [words[random.randint(0, len(words) - 1)] for _ in range(sentence_length)]
+    sentence = " ".join(random_words) 
+
+    return f"<p>{sentence}</p>"
 
 if __name__ == "__main__":
     """To run the Flask server, execute `python app.py` in your terminal.

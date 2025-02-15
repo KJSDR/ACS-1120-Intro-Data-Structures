12f3ec4bf7648f4ae1e3cfbead52a2dc09091aff
Killian <killian.delamarre-ruer@students.dominican.edu>
2025-02-14 17:01:36 -0800

    linkedlist

--- Code/linkedlist.py
+++ Code/linkedlist.py
@@ -1,5 +1,6 @@
 #!python
 
+
 class Node(object):
 
     def __init__(self, data):
@@ -18,6 +19,7 @@ class LinkedList:
         """Initialize this linked list and append the given items, if any."""
         self.head = None  # First node
         self.tail = None  # Last node
+        # Append given items
         if items is not None:
             for item in items:
                 self.append(item)
@@ -30,74 +32,56 @@ class LinkedList:
         return ll_str
 
     def items(self):
-        """Return a list of all items in this linked list."""
-        items = []
-        node = self.head
-        while node is not None:
-            items.append(node.data)
-            node = node.next
-        return items
+        """Return a list (dynamic array) of all items in this linked list.
+        Best and worst case running time: O(n) for n items in the list (length)
+        because we always need to loop through all n nodes to get each item."""
+        items = []  # O(1) time to create empty list
+        # Start at head node
+        node = self.head  # O(1) time to assign new variable
+        # Loop until node is None, which is one node too far past tail
+        while node is not None:  # Always n iterations because no early return
+            items.append(node.data)  # O(1) time (on average) to append to list
+            # Skip to next node to advance forward in linked list
+            node = node.next  # O(1) time to reassign variable
+        # Now list contains items from all nodes
+        return items  # O(1) time to return list
 
     def is_empty(self):
         """Return a boolean indicating whether this linked list is empty."""
         return self.head is None
 
     def length(self):
-        """Return the length of this linked list."""
-        count = 0
-        node = self.head
-        while node is not None:
-            count += 1
-            node = node.next
-        return count
+        """Return the length of this linked list by traversing its nodes.
+        TODO: Running time: O(n) Why and under what conditions?"""
+        # TODO: Loop through all nodes and count one for each
 
     def append(self, item):
-        """Insert the given item at the tail of this linked list."""
-        new_node = Node(item)
-        if self.is_empty():
-            self.head = new_node
-            self.tail = new_node
-        else:
-            self.tail.next = new_node
-            self.tail = new_node
+        """Insert the given item at the tail of this linked list.
+        TODO: Running time: O(???) Why and under what conditions?"""
+        # TODO: Create new node to hold given item
+        # TODO: If self.is_empty() == True set the head and the tail to the new node
+        # TODO: Else append node after tail
 
     def prepend(self, item):
-        """Insert the given item at the head of this linked list."""
-        new_node = Node(item)
-        if self.is_empty():
-            self.head = new_node
-            self.tail = new_node
-        else:
-            new_node.next = self.head
-            self.head = new_node
+        """Insert the given item at the head of this linked list.
+        TODO: Running time: O(???) Why and under what conditions?"""
+        # TODO: Create new node to hold given item
+        # TODO: Prepend node before head, if it exists
 
     def find(self, matcher):
-        """Return an item from this linked list if it is present."""
-        node = self.head
-        while node is not None:
-            if matcher(node.data):
-                return node.data
-            node = node.next
-        return None
+        """Return an item from this linked list if it is present.
+        TODO: Best case running time: O(???) Why and under what conditions?
+        TODO: Worst case running time: O(???) Why and under what conditions?"""
+        # TODO: Loop through all nodes to find item, if present return True otherwise False
 
     def delete(self, item):
-        """Delete the given item from this linked list, or raise ValueError."""
-        node = self.head
-        prev = None
-        while node is not None:
-            if node.data == item:
-                if prev is None:
-                    self.head = node.next
-                    if node.next is None:
-                        self.tail = None
-                else:
-                    prev.next = node.next
-                    if node.next is None:
-                        self.tail = prev
-                return
-            prev = node
-            node = node.next
-        raise ValueError(f'Item not found: {item}')
+        """Delete the given item from this linked list, or raise ValueError.
+        TODO: Best case running time: O(???) Why and under what conditions?
+        TODO: Worst case running time: O(???) Why and under what conditions?"""
+        # TODO: Loop through all nodes to find one whose data matches given item
+        # TODO: Update previous node to skip around node with matching data
+        # TODO: Otherwise raise error to tell user that delete has failed
+        # Hint: raise ValueError('Item not found: {}'.format(item))
 
 
 def test_linked_list():
@@ -113,15 +97,18 @@ def test_linked_list():
     print('tail: {}'.format(ll.tail))
     print('length: {}'.format(ll.length()))
 
-    print('\nTesting delete:')
-    for item in ['B', 'C', 'A']:
-        print('delete({!r})'.format(item))
-        ll.delete(item)
-        print('list: {}'.format(ll))
+    # Enable this after implementing delete method
+    delete_implemented = False
+    if delete_implemented:
+        print('\nTesting delete:')
+        for item in ['B', 'C', 'A']:
+            print('delete({!r})'.format(item))
+            ll.delete(item)
+            print('list: {}'.format(ll))
 
-    print('head: {}'.format(ll.head))
-    print('tail: {}'.format(ll.tail))
-    print('length: {}'.format(ll.length()))
+        print('head: {}'.format(ll.head))
+        print('tail: {}'.format(ll.tail))
+        print('length: {}'.format(ll.length()))
 
 
 if __name__ == '__main__':

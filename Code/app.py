1a036eeaa6915a2a6224008d7e7ca55725a29d45
Killian <killian.delamarre-ruer@students.dominican.edu>
2025-02-18 21:12:20 -0800

    whatever

--- Code/app.py
+++ Code/app.py
@@ -1,5 +1,4 @@
 """Main script, uses other modules to generate sentences."""
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from flask import Flask
 from modules.histogram import histogram
 import random
